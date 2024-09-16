11fdd3a51b2a392709a5fc270d6e4f5ac68d56ad
Ryan Seo <51132357+rseo0301@users.noreply.github.com>
2024-09-16 02:30:49 -0600

    Testing Find and Replace, and added Custom Stamps.

--- SyncfusionPDFViewer/MainWindow.xaml.cs
+++ SyncfusionPDFViewer/MainWindow.xaml.cs
@@ -18,6 +18,10 @@ using System.Windows.Shapes;
 using System.Xml.Linq;
 using System.IO;
 using System.Windows.Controls.Primitives;
+using Syncfusion.Pdf.Redaction;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Color = System.Drawing.Color;
 
 namespace SyncfusionPDFViewer
 {
@@ -33,44 +37,46 @@ namespace SyncfusionPDFViewer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            AddButton();
+            RedactButton();
+            AddTextButton();
+            FindButton();
             HideVerticalToolbar();
             ExpandAnnotationToolbar();
             HideToolbarItems();
+            AddStamps();
         }
-
-        private void AddButton()
+        private void AddTextButton()
         {
             DocumentToolbar toolbar = pdfViewer.Template.FindName("PART_Toolbar", pdfViewer) as DocumentToolbar;
 
             // Create the custom unload button.
             Button button = new Button();
             button.Margin = new Thickness(10, 0, 0, 0);
-            button.Width = 48;
+            button.Width = 60;
             button.Height = 24;
-            button.Content = "Unload";
+            button.Content = "Load Text";
             button.Background = System.Windows.Media.Brushes.Transparent;
             button.BorderThickness = new Thickness(0, 0, 0, 0);
 
             // wire the click event.
-            button.Click += Button_Click;
+            button.Click += Add_Text_Button_Click;
 
             // Define stackPanel and annotationPanel
-            StackPanel stackPanel = (StackPanel)toolbar.Template.FindName("PART_ToolbarStack", toolbar);
+            // StackPanel stackPanel = (StackPanel)toolbar.Template.FindName("PART_ToolbarStack", toolbar);
             StackPanel annotationPanel = (StackPanel)toolbar.Template.FindName("PART_AnnotationsStack", toolbar);
 
             // Add button, can choose to add to stackPanel or annotationPanel
             // stackPanel.Children.Add(button);
             annotationPanel.Children.Add(button);
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Add_Text_Button_Click(object sender, RoutedEventArgs e)
         {
             // Define page and add text programmatically
             PdfLoadedDocument doc = pdfViewer.LoadedDocument;
             PdfLoadedPage page = doc.Pages[0] as PdfLoadedPage;
             PdfGraphics graphics = page.Graphics;
             PdfFont graphicFont = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
-            graphics.DrawString("Testing 123", graphicFont, PdfBrushes.Black, new PointF(0, 772));
+            graphics.DrawString("Test Text", graphicFont, PdfBrushes.Black, new PointF(0, 772));
 
             // Saves pdf to memory and reloads with change
             // Doesn't save PDF
@@ -80,6 +86,122 @@ namespace SyncfusionPDFViewer
             doc.Dispose();
             pdfViewer.Load(stream);
         }
+        private void RedactButton()
+        {
+            DocumentToolbar toolbar = pdfViewer.Template.FindName("PART_Toolbar", pdfViewer) as DocumentToolbar;
+
+            // Create the custom unload button.
+            Button button = new Button();
+            button.Margin = new Thickness(10, 0, 0, 0);
+            button.Width = 60;
+            button.Height = 24;
+            button.Content = "Redact";
+            button.Background = System.Windows.Media.Brushes.Transparent;
+            button.BorderThickness = new Thickness(0, 0, 0, 0);
+
+            button.Click += Redact_Button_Click;
+
+            StackPanel annotationPanel = (StackPanel)toolbar.Template.FindName("PART_AnnotationsStack", toolbar);
+            annotationPanel.Children.Add(button);
+        }
+        private void Redact_Button_Click(object sender, RoutedEventArgs e)
+        {
+            pdfViewer.PageRedactor.EnableRedactionMode = true;
+            pdfViewer.RedactionSettings.FillColor = Colors.White;
+            pdfViewer.FreeTextAnnotationSettings.BorderColor = Colors.White;
+        }
+        // Find these stamps under the stamp icon >> 'Standard Business' icon
+        private void AddStamps()
+        {
+            // To add new stamps, add to the folder 'Stamps' and add file name to folder.
+            string[] files =
+            {
+                "Axis.png",
+                "CArrow.png",
+                "LArrow.png",
+                "RArrow.png",
+                "Table.png",
+                "UArrow.png",
+            };
+
+            foreach (string file in files) 
+            {
+                System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                image.Source = new BitmapImage(new Uri(@"..\..\Stamps\" + file, UriKind.RelativeOrAbsolute));
+                PdfStampAnnotation newStandardStamp = new PdfStampAnnotation(image);
+                pdfViewer.ToolbarSettings.StampAnnotations.Add(newStandardStamp);
+
+            }
+        }
+        private void FindButton()
+        {
+            DocumentToolbar toolbar = pdfViewer.Template.FindName("PART_Toolbar", pdfViewer) as DocumentToolbar;
+
+            // Create the custom unload button.
+            Button button = new Button();
+            button.Margin = new Thickness(10, 0, 0, 0);
+            button.Width = 48;
+            button.Height = 24;
+            button.Content = "Find";
+            button.Background = System.Windows.Media.Brushes.Transparent;
+            button.BorderThickness = new Thickness(0, 0, 0, 0);
+
+            button.Click += Find_Button_Click;
+
+            StackPanel annotationPanel = (StackPanel)toolbar.Template.FindName("PART_AnnotationsStack", toolbar);
+            annotationPanel.Children.Add(button);
+        }
+
+        // This is currently tries to find and replace text. Unfortunately, it doesn't always work.
+        // 1) It doesn't always find the text.
+        // 2) The length of the old and new text needs to similar for a clean redaction.
+        private void Find_Button_Click(object sender, RoutedEventArgs e)
+        {
+            PdfLoadedDocument doc = pdfViewer.LoadedDocument;
+            PdfLoadedPage data = doc.Pages[0] as PdfLoadedPage;
+            Dictionary<int, List<RectangleF>> matchedTextbounds = new Dictionary<int, List<RectangleF>>();
+
+            string text = "Hello";
+            pdfViewer.FindText(text, out matchedTextbounds);
+
+            // Iterate through the matchedTextbounds dictionary.
+            foreach (KeyValuePair<int, List<RectangleF>> matchedText in matchedTextbounds)
+            {
+                //Create a font to draw the replacement text.
+                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 12);
+                int pageIndex = matchedText.Key; // Get the page index
+                List<RectangleF> rectangles = matchedText.Value;
+                // Access the page using the page index
+                PdfLoadedPage page = doc.Pages[pageIndex] as PdfLoadedPage;
+
+                // Now, you can work with the page as needed.
+                for (int j = 0; j < rectangles.Count; j++)
+                {
+                    //Create a PDF redaction and set the fill color as transparent.
+                    PdfRedaction redaction = new PdfRedaction(rectangles[j], Color.Transparent);
+                    //Draw the replacement text on the redaction appearance.
+                    redaction.Appearance.Graphics.DrawString("New Text", font, PdfBrushes.Black, PointF.Empty);
+                    page.AddRedaction(redaction);
+                    // page.Graphics.DrawRectangle(PdfBrushes.White, rectangles[j]);
+                }
+            }
+
+            doc.Redact();
+
+            // Saves pdf to memory and reloads with change
+            // Doesn't save PDF
+            MemoryStream stream = new MemoryStream();
+            doc.Save(stream);
+            doc.Close();
+            doc.Dispose();
+            pdfViewer.Load(stream);
+
+            //TESTING
+            //Get the occurrences of the target text and location.
+            //Highlights in UI. Currently unable to access programatically.
+            //pdfViewer.SearchText("TEST");
+        }
+
         private void HideVerticalToolbar()
         {
             // Hides the thumbnail icon. 
@@ -104,7 +226,6 @@ namespace SyncfusionPDFViewer
             // Expand the annotation toolbar. 
             annotationButton.IsChecked = true;
         }
-
         private void HideToolbarItems()
         {
             // https://help.syncfusion.com/wpf/pdf-viewer/how-to/disable-toolbar-items
